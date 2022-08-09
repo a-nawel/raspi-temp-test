87f95865f2fbab9917ba102a32a25e5a2c20385c
a-nawel <91109440+a-nawel@users.noreply.github.com>
2022-08-09 09:56:53 -0700

    Increase the calculation time to 30 min

--- stress-test.sh
+++ stress-test.sh
@@ -4,7 +4,7 @@
 ./temp-sampling.sh >/dev/null 2>&1 &
 
 # Calculate the value of pi for 20 min 
-stress-ng --cpu 0 --cpu-method pi -t 20m
+stress-ng --cpu 0 --cpu-method pi -t 30m
 
 # Kill the sampling process
 killall temp-sampling.sh
